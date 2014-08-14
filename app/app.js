b306e99cedfbcbe9218452b4292da05345b4a679
Demian Borba <demian.borba@actioncreations.com>
2014-08-14 20:25:17 -0300

    Add routing

--- app/app.js
+++ app/app.js
@@ -1,3 +1,18 @@
 'use strict';
 
-angular.module('07ShoppingFlash', ['sf.greet']);
+angular.module('ShoppingFlash', ['ngRoute'])
+    .config(['$routeProvider',
+    function (routeProvider) {
+        routeProvider.
+        when('/products', {
+            templateUrl: 'partials/main.html',
+            controller: 'MainCtrl'
+        }).
+        when('/products/:id', {
+            templateUrl: 'partials/productDetails.html',
+            controller: 'ProductDetailsCtrl'
+        }).
+        otherwise({
+            redirectTo: '/products'
+        });
+}]);
