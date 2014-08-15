5875dccef411ae5fae5816e1d4ea93769f1103e1
Demian Borba <demian.borba@actioncreations.com>
2014-08-15 18:44:05 -0300

    clean all to start fresh

--- app/controllers/mainCtrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ShoppingFlash')
-    .controller('MainCtrl', function($scope, $location, ProductsService){
-        $scope.products = ProductsService.getProducts();
-        $scope.routeTo = function (id) {
-           $location.path('/product/' + id);
-        }
-});
--- app/controllers/productDetailsCtrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('ShoppingFlash')
-    .controller('ProductDetailsCtrl', function($scope, $routeParams){
-        
-});
